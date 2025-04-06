fd6f8bb1b28ae66ea7cf353ae9b9f8223e85e2cb
tin2003tin <tinsiriwid@gmail.com>
2025-04-06 18:20:26 +0700

    update app.js and app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,14 @@
 const { dayOfTheWeek } = require('./app');
+const { monthOfTheYear } = require('./app');
 
 
 test('getDay returns the long-format day of the week', () => {
-    const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('Wednesday');
+    const day = dayOfTheWeek(new Date('3/11/2020'));
+    expect(day).toBe('Wednesday');
+});
+
+
+test('getDay returns the month of the year', () => {
+    const month = dayOfTheWeek(new Date('3/11/2020'));
+    expect(month).toBe('March');
 });
